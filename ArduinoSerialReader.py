442125e3be06fff26d3045367ec6a3dcf4183f6b
Gustavo Palacios <gpalacios82@gmail.com>
2018-12-06 00:47:17 +0100

    Añadido control de errores

--- ArduinoSerialReader.py
+++ ArduinoSerialReader.py
@@ -1,15 +1,18 @@
 import serial
 
 if __name__ == "__main__":
-    print('Conectando puerto serie . . .')
-    ser = serial.Serial('/dev/cu.usbmodem14201', 9600)
+    try:
+        print('Conectando puerto serie . . .')
+        ser = serial.Serial('/dev/cu.usbmodem14201', 9600)
 
-    print('Leyendo puerto serie . . .')
-    buffer = str(ser.readline())
+        print('Leyendo puerto serie . . .')
+        buffer = str(ser.readline())
 
-    position_match = buffer.find(';')
-    temperature = buffer[2:position_match]
-    humidity = buffer[position_match+1:buffer.__len__()-5]
+        position_match = buffer.find(';')
+        temperature = buffer[2:position_match]
+        humidity = buffer[position_match+1:buffer.__len__()-5]
 
-    print('La temperatura es de: ' + str(temperature) + " grados")
-    print('La humedad es de: ' + str(humidity) + " %")
+        print('La temperatura es de: ' + str(temperature) + " grados")
+        print('La humedad es de: ' + str(humidity) + " %")
+    except:
+        print('Puerto serie no disponible')
