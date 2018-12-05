5aeadf2888f44d9fd39dbddbbe19e9ffc9330ecc
Gus <Gus@Guss-Macintosh-2.local>
2018-12-06 00:31:09 +0100

    v0.1 Alpha

---
+++ ArduinoSerialReader.py
@@ -0,0 +1,15 @@
+import serial
+
+if __name__ == "__main__":
+    print('Conectando puerto serie . . .')
+    ser = serial.Serial('/dev/cu.usbmodem14201', 9600)
+
+    print('Leyendo puerto serie . . .')
+    buffer = str(ser.readline())
+
+    position_match = buffer.find(';')
+    temperature = buffer[2:position_match]
+    humidity = buffer[position_match+1:buffer.__len__()-5]
+
+    print('La temperatura es de: ' + str(temperature) + " grados")
+    print('La humedad es de: ' + str(humidity) + " %")
