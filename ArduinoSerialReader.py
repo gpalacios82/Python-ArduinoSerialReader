757b579f11100fbdbd590d9f3462106462d29115
Gustavo Palacios <gpalacios82@gmail.com>
2018-12-06 21:07:36 +0100

    v1.2 Añadida información de Voltaje y Luminosidad

--- ArduinoSerialReader.py
+++ ArduinoSerialReader.py
@@ -8,11 +8,29 @@ if __name__ == "__main__":
         print('Leyendo puerto serie . . .')
         buffer = str(ser.readline())
 
-        position_match = buffer.find(';')
-        temperature = buffer[2:position_match]
-        humidity = buffer[position_match+1:buffer.__len__()-5]
+        print(" ")
+        #print("Leido: " + str(buffer))
+
+        position_match1 = buffer.find('Temp:')
+        position_match2 = buffer.find('\\xc2')
+        temperature = buffer[position_match1+5:position_match2]
+
+        position_match1 = buffer.find('Humidity:')
+        position_match2 = buffer.find('%;Luminosity')
+        humidity = buffer[position_match1+9:position_match2]
+
+        position_match1 = buffer.find('Luminosity:')
+        position_match2 = buffer.find('%;Voltaje')
+        luminosity = buffer[position_match1+11:position_match2]
+
+        position_match1 = buffer.find('Voltaje:')
+        position_match2 = buffer.find('V\\r\\n')
+        voltaje = buffer[position_match1+8:position_match2]
+
 
         print('La temperatura es de: ' + str(temperature) + " grados")
-        print('La humedad es de: ' + str(humidity) + " %")
+        print('La humedad es del: ' + str(humidity) + " %")
+        print('La luminosidad es del: ' + str(luminosity) + " %")
+        print('El voltaje de la placa es de: ' + str(voltaje) + " V")
     except:
         print('Puerto serie no disponible')
--- README.md
+++ README.md
@@ -3,6 +3,25 @@ Conectamos mediante python a la placa arduino con sensor de temperatura y humeda
 
 Básicamente leemos del puerto serie y separamos los parámetros que vienen.
 
-En esta primera versión:
+##### En esta versión:
 * Humedad
 * Temperatura
+* Luminosidad
+* Voltaje
+
+El buffer de entrada desde el puerto serie es:
+b'Temp:21.00\xc2\xbaC;Humidity:55.00%;Luminosity:98.00%;Voltaje:4.72V\r\n'
+
+##### Ejemplo de Salida
+Conectando puerto serie . . .
+
+Leyendo puerto serie . . .
+
+ 
+La temperatura es de: 23.00 grados
+
+La humedad es del: 43.00 %
+
+La luminosidad es del: 98.00 %
+
+El voltaje de la placa es de: 4.72 V
